0b861f10f528f6bbd302f3923026f924902167a5
trung <trung.n.k@gmail.com>
2009-10-02 10:42:27 +0800

    can parse amf3, binary version of amf_requets.xml

--- temp/server/amf3.erl
+++ temp/server/amf3.erl
@@ -29,6 +29,7 @@ reset() ->
 clear(TableName) ->
     try ets:delete_all_objects(TableName) of
 	true ->
+	    _ = ets:insert(TableName, {counter, 0}),
 	    {ok}
     catch
 	error:Error ->
@@ -49,7 +50,7 @@ sure_exist(TableName) ->
 %% wrapper of ets:insert, just make sure the table exists
 insert(TableName, Obj) ->
     _ = sure_exist(TableName),
-    {ok, {counter, Count}} = read(TableName, counter),
+    {ok, Count} = read(TableName, counter),
     io:fwrite("Inserting into ~p - index: ~p - value: ~p ~n", [TableName, Count, Obj]),
     _ = ets:insert(TableName, {Count, Obj}),
     _ = ets:insert(TableName, {counter, Count+1}),
@@ -57,10 +58,14 @@ insert(TableName, Obj) ->
 
 %% wrapper of ets:lookup
 read(TableName, Ref) ->
+    io:fwrite("Read from ~p with index ~p got value ", [TableName, Ref]),
     case ets:lookup(TableName, Ref) of
 	[] ->
 	    {bad, {not_found, TableName, Ref}};
-	[Obj|_] ->
+	[{counter, Count}|_] ->
+	    {ok, Count};
+	[{_Idx, Obj}|_] ->
+%%	    io:fwrite("~p~n", [Obj]),
 	    {ok, Obj}
     end.
 
